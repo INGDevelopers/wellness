614848b9d4f8630679cf0427bc217b1b471db7f0
Andres Quintero <andresfelipequintero2091gmail.com>
2022-10-11 20:24:27 -0500

    Añadiendo ruta login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom';
+
+
 import Login from './components/login';
 import './App.css';
 
+
+
 function App() {
   return (
-    <div className="App">
-      <div className="img"></div>
-      <Login></Login>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
+    </Router>
   );
 }
 
