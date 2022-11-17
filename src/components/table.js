f759c4c6ae04023a1f5ed8cc8936127e0ab2fecb
FILIPRAIDER <filipraider123@gmail.com>
2022-11-17 01:10:59 -0500

    creacion del modal para las solicitudes

--- src/components/table.js
+++ src/components/table.js
@@ -3,24 +3,24 @@ import "./styles/table.css";
 
 function History(props) {
 
-	return (
-		<> 
-			<div className="History">
-                
-				<table>
-					<thead>
-						<tr>
+    return (
+        <>
+            <div className="History">
+
+                <table>
+                    <thead>
+                        <tr>
                             {
                                 props.title.map((ele, i) => (
                                     <th key={i}>{ele}</th>
                                 ))
                             }
-						</tr>
-					</thead>
+                        </tr>
+                    </thead>
 
                     <tbody>
                         {
-                            props.table === 'users'?
+                            props.table === 'users' ?
                                 props.field.map((ele, i) => (
                                     <tr key={i}>
                                         <td>Vacio</td>
@@ -32,35 +32,35 @@ function History(props) {
                                         </td>
                                     </tr>
                                 ))
-                            :props.table === 'requests'?
-                                props.field.map((ele, i) => (
-                                    <tr key={i}>
-                                        <td>Vacio</td>
-                                        <td>{ele.idUser.email}</td>
-                                        <td>{ele.idUser.name1}</td>
-                                        <td>{ele.idInstrument.name}</td>
-                                        <td>{ele.requ}</td>
-                                        <td>{ele.createdAt}</td>
-                                        <td>{ele.updatedAt}</td>
-                                        <td><button className="button-primary">Ver</button></td>
-                                    </tr>
-                                ))
-                            :props.table === 'history'?
-                                props.field.map((ele, i) => (
-                                    <tr key={i}>
-                                        <td>Vacio</td>
-                                        <td>{ele.idInstrument.name}</td>
-                                        <td>{ele.createdAt}</td>
-                                        <td>{ele.updatedAt}</td>
-                                    </tr>
-                                ))
-                            :null
+                                : props.table === 'requests' ?
+                                    props.field.map((ele, i) => (
+                                        <tr key={i}>
+                                            <td>Vacio</td>
+                                            <td>{ele.idUser.email}</td>
+                                            <td>{ele.idUser.name1}</td>
+                                            <td>{ele.idInstrument.name}</td>
+                                            <td>{ele.requ}</td>
+                                            <td>{ele.createdAt}</td>
+                                            <td>{ele.updatedAt}</td>
+                                            <td><button className="button-primary" onClick={props.onClick}>Ver</button></td>
+                                        </tr>
+                                    ))
+                                    : props.table === 'history' ?
+                                        props.field.map((ele, i) => (
+                                            <tr key={i}>
+                                                <td>Vacio</td>
+                                                <td>{ele.idInstrument.name}</td>
+                                                <td>{ele.createdAt}</td>
+                                                <td>{ele.updatedAt}</td>
+                                            </tr>
+                                        ))
+                                        : null
                         }
                     </tbody>
-				</table>
-			</div>
-		</>
-	);
+                </table>
+            </div>
+        </>
+    );
 }
 
 export default History;
--- src/containers/request.js
+++ src/containers/request.js
@@ -1,20 +1,20 @@
 import { useState, useEffect } from 'react';
 import Table from '../components/table';
-
+import ModalR from "../components/modalR";
 import requests from '../config/requests';
-
+import Request from '../components/Request';
 const Requests = () => {
-  const title = ['Perfil', 'Correo', 'Nombre', 'Instrumento', 'Solicitud', 'Creacion', 'Modificacion', 'Autorizacion'];
-  const [listRequests, setListRequests] = useState([]);
+	const title = ['Perfil', 'Correo', 'Nombre', 'Instrumento', 'Solicitud', 'Creacion', 'Modificacion', 'Autorizacion'];
+	const [listRequests, setListRequests] = useState([]);
+	const [show, setShow] = useState(false);
+	useEffect(() => {
+		requests.getRequests().then((res) => {
+			setListRequests(res.res);
+		});
+	}, []);
 
-  useEffect(() => {
-    requests.getRequests().then((res) =>{
-      setListRequests(res.res);
-    });
-  },[]);
-    
 
-  return (
+	return (
 		<>
 			<div className='div-principal-admin'>
 				{/* <div className="div-button-a">
@@ -28,8 +28,12 @@ const Requests = () => {
 						Este historial contiene la informacion de todas las solicitudes generadas por los estudiantes
 					</h6>
 				</div>
-				<Table table={'requests'} title={title} field={listRequests} />
+
+				<Table table={'requests'} title={title} field={listRequests} onClick={() => setShow(true)} />
+
 			</div>
+
+			<ModalR show={show} onHide={() => setShow(false)} element={<Request />} />
 		</>
 	);
 }
