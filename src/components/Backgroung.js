d2dcc63c4f555dd96ec142c5c40426028715a532
FILIPRAIDER <filipraider123@gmail.com>
2022-11-24 16:31:33 -0500

    editando el diseño de la pagina

--- src/components/Backgroung.js
+++ src/components/Backgroung.js
@@ -4,7 +4,7 @@ import futbol from '../icons/futbol.png';
 import guitarra from '../icons/guitarra.png';
 import raqueta from '../icons/raqueta.png';
 import volley from '../icons/volley.png';
-
+import card from '../icons/cards.png';
 
 const Backgroung = () => {
     return (
@@ -15,20 +15,38 @@ const Backgroung = () => {
                 <img id='guitarra' src={guitarra} alt="guitarra img" />
                 <img id='raqueta' src={raqueta} alt="raqueta img" />
                 <img id='volley' src={volley} alt="volley img" />
+                <img id='cards' src={card} alt="cards img" />
+                <img id='basket2' src={basket} alt="basket img" />
+                <img id='futbol2' src={futbol} alt="futbol img" />
+                <img id='guitarra2' src={guitarra} alt="guitarra img" />
+                <img id='raqueta2' src={raqueta} alt="raqueta img" />
+                <img id='volley2' src={volley} alt="volley img" />
             </div>
             <div className='top-right'>
                 <img id='basket' src={basket} alt="basket img" />
-                <img id='futbol' src={futbol} alt="futbol img" />
                 <img id='guitarra' src={guitarra} alt="guitarra img" />
                 <img id='raqueta' src={raqueta} alt="raqueta img" />
                 <img id='volley' src={volley} alt="volley img" />
+                <img id='cards' src={card} alt="cards img" />
+                <img id='cards2' src={card} alt="cards img" />
+                <img id='basket2' src={basket} alt="basket img" />
+                <img id='futbol2' src={futbol} alt="futbol img" />
+                <img id='guitarra2' src={guitarra} alt="guitarra img" />
+                <img id='raqueta2' src={raqueta} alt="raqueta img" />
+                <img id='volley2' src={volley} alt="volley img" />
             </div>
             <div className='bottom-left'>
                 <img id='basket' src={basket} alt="basket img" />
-                <img id='futbol' src={futbol} alt="futbol img" />
                 <img id='guitarra' src={guitarra} alt="guitarra img" />
                 <img id='raqueta' src={raqueta} alt="raqueta img" />
                 <img id='volley' src={volley} alt="volley img" />
+                <img id='cards' src={card} alt="cards img" />
+                <img id='cards2' src={card} alt="cards img" />
+                <img id='basket2' src={basket} alt="basket img" />
+                <img id='futbol2' src={futbol} alt="futbol img" />
+                <img id='guitarra2' src={guitarra} alt="guitarra img" />
+                <img id='raqueta2' src={raqueta} alt="raqueta img" />
+                <img id='volley2' src={volley} alt="volley img" />
             </div>
             <div className='bottom-right'>
                 <img id='basket' src={basket} alt="basket img" />
@@ -36,6 +54,13 @@ const Backgroung = () => {
                 <img id='guitarra' src={guitarra} alt="guitarra img" />
                 <img id='raqueta' src={raqueta} alt="raqueta img" />
                 <img id='volley' src={volley} alt="volley img" />
+                <img id='cards' src={card} alt="cards img" />
+                <img id='basket2' src={basket} alt="basket img" />
+                <img id='futbol2' src={futbol} alt="futbol img" />
+                <img id='guitarra2' src={guitarra} alt="guitarra img" />
+                <img id='raqueta2' src={raqueta} alt="raqueta img" />
+                <img id='volley2' src={volley} alt="volley img" />
+
             </div>
         </div>
     )
