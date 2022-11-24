a61d051fa1d219664c084eabdc8f907a8bb1f3ef
FILIPRAIDER <filipraider123@gmail.com>
2022-11-24 15:02:32 -0500

    mejoras de diseño

--- src/containers/home.js
+++ src/containers/home.js
@@ -7,6 +7,7 @@ import Alert from '../components/alert';
 import instruments from "../config/instruments";
 import users from "../config/users";
 import requests from "../config/requests";
+import Backgroung from "../components/Backgroung";
 
 
 
@@ -22,68 +23,69 @@ function Home() {
 			console.log(res.res)
 		});
 
-		users.getUserById().then((res)=> {
+		users.getUserById().then((res) => {
 			setInUse(res.res.inUse);
 		});
-	},[isLoading]);
+	}, [isLoading]);
 
 	const deleteInUseUser = (id) => {
 		setIsLoading(true);
 		users.deleteInUseUser(id).then((res) => {
-      setInUse(res.res.inUse);
+			setInUse(res.res.inUse);
 			setIsLoading(false);
-    });
+		});
 	}
 
 	const requestInstrument = (id) => {
 		setIsLoading(true);
 		requests.create(id).then((res) => {
-			if(res.success)
+			if (res.success)
 				console.log('Elemento Solicitado')
-				setIsLoading(false);
+			setIsLoading(false);
 		})
 	}
 
-	
+
 	return (
 		<>
 			<NavBar />
-			
-			<div className="div-principal">
-					<div className="div-button-a">
-									<a className="button-primary" href="/history">Historial de Uso</a>
-					</div>
+			<Backgroung />
 
-					<div className="title">
-						<h2>Instrumentos</h2>
-						<h6>Disponibles en almacen</h6>
-					</div>
-					<div className="div-home-items">
-						{
-							data.length?
-								// Instrumentos en inventario
-								data.map((el, i) => (
-									<CardI key={i} title={el.name} disp={el.amount} inUse={el.inUse} contentType={el.img.contentType} data={el.img.data} 
-										buttonR={<button  className="button-primary" onClick={() => requestInstrument(el._id)}>Solicitar</button>}
-									/>
-								))
-							: 
-							<Alert/>
-						}
-					</div>
-					<div className="title">
-						<h2>Instrumentos en uso</h2>
-					</div>
-					<div className="div-home-items">
-						{
+			<div className="div-principal">
+				<div className="div-button-a">
+					<a className="button-primary" href="/history">Historial de Uso</a>
+				</div>
+
+				<div className="title">
+					<h2>Instrumentos</h2>
+					<h6>Disponibles en almacen</h6>
+				</div>
+				<div className="div-home-items">
+					{
+						data.length ?
 							// Instrumentos en inventario
-							inUse.map((el, i) => (
-								<CardI key={i} title={el.idInstrument.name} disp={el.disp} inUse={el.inUse} 
-									buttonC={inUse.length?<button className="button-primary" style={{ backgroundColor: "red", borderColor: "red" }} onClick={() => deleteInUseUser(el.idInstrument._id)}>Devolver</button>:null}
+							data.map((el, i) => (
+								<CardI key={i} title={el.name} disp={el.amount} inUse={el.inUse} contentType={el.img.contentType} data={el.img.data}
+									buttonR={<button className="button-primary" onClick={() => requestInstrument(el._id)}>Solicitar</button>}
 								/>
 							))
-						}
-					</div>
+							:
+							<Alert />
+					}
+				</div>
+				<div className="title">
+					<h2>Instrumentos en uso</h2>
+				</div>
+				<div className="div-home-items">
+					{
+						// Instrumentos en inventario
+						inUse.map((el, i) => (
+							<CardI key={i} title={el.idInstrument.name} disp={el.disp} inUse={el.inUse}
+								buttonC={inUse.length ? <button className="button-primary" style={{ backgroundColor: "red", borderColor: "red" }} onClick={() => deleteInUseUser(el.idInstrument._id)}>Devolver</button> : null}
+							/>
+						))
+					}
+				</div>
 			</div>
 		</>
 	);
