71d8f3fad24e037f210b02c0813bfec3de1959aa
FILIPRAIDER <filipraider123@gmail.com>
2022-11-25 10:34:22 -0500

    optimizacion del fondo

--- src/components/Backgroung.js
+++ src/components/Backgroung.js
@@ -1,65 +1,27 @@
 import './styles/Background.css';
-import basket from '../icons/basket.png';
-import futbol from '../icons/futbol.png';
-import guitarra from '../icons/guitarra.png';
-import raqueta from '../icons/raqueta.png';
-import volley from '../icons/volley.png';
-import card from '../icons/cards.png';
+import topleft from '../images/1.png';
+import topright from '../images/2.png';
+import bottomleft from '../images/3.png';
+import bottomright from '../images/4.png';
 
 const Backgroung = () => {
     return (
         <div className='main-container'>
             <div className='top-left'>
-                <img id='basket' src={basket} alt="basket img" />
-                <img id='futbol' src={futbol} alt="futbol img" />
-                <img id='guitarra' src={guitarra} alt="guitarra img" />
-                <img id='raqueta' src={raqueta} alt="raqueta img" />
-                <img id='volley' src={volley} alt="volley img" />
-                <img id='cards' src={card} alt="cards img" />
-                <img id='basket2' src={basket} alt="basket img" />
-                <img id='futbol2' src={futbol} alt="futbol img" />
-                <img id='guitarra2' src={guitarra} alt="guitarra img" />
-                <img id='raqueta2' src={raqueta} alt="raqueta img" />
-                <img id='volley2' src={volley} alt="volley img" />
+                <img id='topleft' src={topleft} alt="basket img" />
+
             </div>
             <div className='top-right'>
-                <img id='basket' src={basket} alt="basket img" />
-                <img id='guitarra' src={guitarra} alt="guitarra img" />
-                <img id='raqueta' src={raqueta} alt="raqueta img" />
-                <img id='volley' src={volley} alt="volley img" />
-                <img id='cards' src={card} alt="cards img" />
-                <img id='cards2' src={card} alt="cards img" />
-                <img id='basket2' src={basket} alt="basket img" />
-                <img id='futbol2' src={futbol} alt="futbol img" />
-                <img id='guitarra2' src={guitarra} alt="guitarra img" />
-                <img id='raqueta2' src={raqueta} alt="raqueta img" />
-                <img id='volley2' src={volley} alt="volley img" />
+                <img id='topright' src={topright} alt="basket img" />
+
             </div>
             <div className='bottom-left'>
-                <img id='basket' src={basket} alt="basket img" />
-                <img id='guitarra' src={guitarra} alt="guitarra img" />
-                <img id='raqueta' src={raqueta} alt="raqueta img" />
-                <img id='volley' src={volley} alt="volley img" />
-                <img id='cards' src={card} alt="cards img" />
-                <img id='cards2' src={card} alt="cards img" />
-                <img id='basket2' src={basket} alt="basket img" />
-                <img id='futbol2' src={futbol} alt="futbol img" />
-                <img id='guitarra2' src={guitarra} alt="guitarra img" />
-                <img id='raqueta2' src={raqueta} alt="raqueta img" />
-                <img id='volley2' src={volley} alt="volley img" />
+                <img id='bottomleft' src={bottomleft} alt="basket img" />
+
             </div>
             <div className='bottom-right'>
-                <img id='basket' src={basket} alt="basket img" />
-                <img id='futbol' src={futbol} alt="futbol img" />
-                <img id='guitarra' src={guitarra} alt="guitarra img" />
-                <img id='raqueta' src={raqueta} alt="raqueta img" />
-                <img id='volley' src={volley} alt="volley img" />
-                <img id='cards' src={card} alt="cards img" />
-                <img id='basket2' src={basket} alt="basket img" />
-                <img id='futbol2' src={futbol} alt="futbol img" />
-                <img id='guitarra2' src={guitarra} alt="guitarra img" />
-                <img id='raqueta2' src={raqueta} alt="raqueta img" />
-                <img id='volley2' src={volley} alt="volley img" />
+                <img id='bottomright' src={bottomright} alt="basket img" />
+
 
             </div>
         </div>
